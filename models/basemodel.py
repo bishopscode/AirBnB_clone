65a5c896696161b36e25a17b8b724c391c84258d
bishopscode <bishopslow01@gmail.com>
2024-02-05 22:06:15 +0100

    more update

---
+++ models/basemodel.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+from datetime import datetime
+import models
+import uuid
+
+
+class BaseModel:
+    """
+    BaseModel class enables creation and instances manager.
+    """
+    def __init__(self, *args, **kwargs):
+        '''
+        Docs
+        '''
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at':
+                    value = datetime.fromisoformat(value)
+                    self.created_at = value
+                elif key == 'updated_at':
+                    value = datetime.fromisoformat(value)
+                    self.updated_at = value
+                elif key == 'id':
+                    self.id = str(value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
+
+    def save(self):
+        '''
+        Docs
+        '''
+        self.updated_at = datetime.now()
+        models.storage.save()
+        models.storage.new(self)
+
+    def __str__(self):
+        '''
+        returns string representatioon of instances
+        '''
+        class_name = type(self).__name__
+
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+    
+
+    def to_dict(self) -> dict:
+        """Return a dictionary of instance attributes."""
+        sect = ['name', 'my_number']
+        dictionary = {k: v for k, v in self.__dict__.items() if k not in sect}
+        dictionary['__class__'] = self.__class__.__name__
+
+        for k, v in dictionary.items():
+            if isinstance(v, datetime):
+                dictionary[k] = v.isoformat()
+
+        return dictionary
+    

c4055a6eb7237bf14a658a9183671d979370577f
bishopscode <64908269+bishopscode@users.noreply.github.com>
2024-02-12 13:43:56 +0100

    Update base_model.py
    ensure serialization and deserailization

--- models/base_model.py
+++ models/base_model.py
@@ -7,9 +7,6 @@ class BaseModel:
     BaseModel class enables creation and instances manager.
     """
     def __init__(self, *args, **kwargs):
-        '''
-        Docs
-        '''
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at':
@@ -22,6 +19,7 @@ class BaseModel:
                     self.id = str(value)
                 else:
                     setattr(self, key, value)
+            models.storage.new(self)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -29,25 +27,14 @@ class BaseModel:
             models.storage.new(self)
 
     def save(self):
-        '''
-        Docs
-        '''
         self.updated_at = datetime.now()
         models.storage.save()
-        models.storage.new(self)
 
     def __str__(self):
-        '''
-        returns string representatioon of instances
-        '''
         class_name = type(self).__name__
-
         return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
 
-    def to_dict(self) -> dict:
-        """
-        Return a dictionary of every instance attributes.
-        """
+    def to_dict(self):
         sect = ['name', 'my_number']
         dictionary = {key: value for key, value in self.__dict__.items() if key not in sect}
         dictionary['__class__'] = self.__class__.__name__

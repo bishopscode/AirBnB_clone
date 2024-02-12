9e6e26b68a95c50629623692742ae1114ef48327
bishopscode <64908269+bishopscode@users.noreply.github.com>
2024-02-12 16:38:18 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 from datetime import datetime
-from models import storage
+from models.engine.file_storage import FileStorage
 import uuid
-
 
 class BaseModel:
     """
@@ -34,14 +33,14 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            storage.new(self)  # Use the storage attribute to add the instance
+            FileStorage().new(self)  # Use the FileStorage instance to add the instance
 
     def save(self):
         '''
         Saves the instance to the storage.
         '''
         self.updated_at = datetime.now()
-        storage.save()  # Use the storage attribute to save the instance
+        FileStorage().save()  # Use the FileStorage instance to save the instance
 
     def __str__(self):
         '''

0acd2aa168252bbd714d9032ad5b07a1ef52317d
bishopscode <64908269+bishopscode@users.noreply.github.com>
2024-02-12 13:28:03 +0100

    Update base_model.py
    fixing dictionary issue

--- models/base_model.py
+++ models/base_model.py
@@ -1,22 +1,59 @@
-import uuid
 from datetime import datetime
+import models
+import uuid
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
-
-    def __str__(self):
-        return "[{}] ({}) {}".format(
-            type(self).__name__, self.id, self.__dict__)
+    """
+    BaseModel class enables creation and instances manager.
+    """
+    def __init__(self, *args, **kwargs):
+        '''
+        Docs
+        '''
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at':
+                    value = datetime.fromisoformat(value)
+                    self.created_at = value
+                elif key == 'updated_at':
+                    value = datetime.fromisoformat(value)
+                    self.updated_at = value
+                elif key == 'id':
+                    self.id = str(value)
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
 
     def save(self):
+        '''
+        Docs
+        '''
         self.updated_at = datetime.now()
+        models.storage.save()
+        models.storage.new(self)
 
-    def to_dict(self):
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = type(self).__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
+    def __str__(self):
+        '''
+        returns string representatioon of instances
+        '''
+        class_name = type(self).__name__
+
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+
+    def to_dict(self) -> dict:
+        """
+        Return a dictionary of every instance attributes.
+        """
+        sect = ['name', 'my_number']
+        dictionary = {key: value for key, value in self.__dict__.items() if key not in sect}
+        dictionary['__class__'] = self.__class__.__name__
+
+        for key, value in dictionary.items():
+            if isinstance(value, datetime):
+                dictionary[key] = value.isoformat()
+
+        return dictionary

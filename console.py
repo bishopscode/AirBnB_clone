8b5de9bb161f5146b761f10e1fde4413cf4e3c51
bishopscode <64908269+bishopscode@users.noreply.github.com>
2024-02-12 12:47:48 +0100

    Update console.py
    properly ensure attribute for storage was properly implemented

--- console.py
+++ console.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
 
-"""The cmd Module.
-for building line-oriented command interpreters
-"""
-import cmd
-from models.base_model import BaseModel
+# main.py
+
 import models
-import re
+from cmd import Cmd
+from models.base_model import BaseModel
 from models.user import User
-import re
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-from datetime import datetime
+from file_storage import FileStorage  # Import FileStorage class
 
+class HBNBCommand(Cmd):
+    prompt = "(hbnb) "
 
-class HBNBCommand(cmd.Cmd):
-    prompt = "(hbnb) "
-    """
-    cmd Module.
-    for building line-oriented command interpreters
-    """
-    prompt = "(hbnb) "
+    def __init__(self):
+        super().__init__()
+        # Instantiate FileStorage and assign it to models.storage
+        models.storage = FileStorage()
 
     def do_quit(self, line):
         """
@@ -50,45 +46,24 @@ class HBNBCommand(cmd.Cmd):
         if len(line) > 0:
             line_array = line.split()
             if len(line_array) > 0:
-                if line_array[0] == "BaseModel":
-                    obj = BaseModel()
-                    obj.save()
-                    print(obj.id)
-                elif line_array[0] == "User":
-                    obj = User()
-                    obj.save()
-                    print(obj.id)
-                elif line_array[0] == "State":
-                    obj = State()
-                    obj.save()
-                    print(obj.id)
-                elif line_array[0] == "City":
-                    obj = City()
-                    obj.save()
-                    print(obj.id)
-                elif line_array[0] == "Amenity":
-                    obj = Amenity()
-                    obj.save()
-                    print(obj.id)
-                elif line_array[0] == "Place":
-                    obj = Place()
-                    obj.save()
-                    print(obj.id)
-                elif line_array[0] == "Review":
-                    obj = Review()
+                class_name = line_array[0]
+                if class_name in models.storage.CLASSES:
+                    obj = models.storage.CLASSES[class_name]()
                     obj.save()
                     print(obj.id)
                 else:
                     print("** class doesn't exist **")
         else:
             print("** class name missing **")
+
     def do_show(self, line):
         """
         Docs
         """
         staged_classes = [
-                "BaseModel", "User", "State",
-                "City", "Amenity", "Place", "Review"]
+            "BaseModel", "User", "State", "City",
+            "Amenity", "Place", "Review"
+        ]
         if len(line) > 0:
             line_array = line.split()
             if len(line_array) > 0:
@@ -97,7 +72,7 @@ class HBNBCommand(cmd.Cmd):
                     if len(line_array) > 1:
                         objs_dict = models.storage.all()
                         scout_string = "{}.{}".format(
-                                class_name, line_array[1])
+                            class_name, line_array[1])
                         if scout_string in objs_dict:
                             print(objs_dict[scout_string])
                         else:
@@ -109,14 +84,15 @@ class HBNBCommand(cmd.Cmd):
         else:
             print("** class name missing **")
 
-def do_destroy(self, line):
+    def do_destroy(self, line):
         """
         Deletes an instance based on the class name and id
         (save the change into the JSON file)
         """
         staged_classes = [
-                "BaseModel", "User", "State",
-                "City", "Amenity", "Place", "Review"]
+            "BaseModel", "User", "State", "City",
+            "Amenity", "Place", "Review"
+        ]
         if len(line) > 0:
             line_array = line.split()
             if len(line_array) > 0:
@@ -125,7 +101,7 @@ def do_destroy(self, line):
                     if len(line_array) > 1:
                         objs_dict = models.storage.all()
                         scout_string = "{}.{}".format(
-                                class_name, line_array[1])
+                            class_name, line_array[1])
                         if scout_string in objs_dict:
                             del (objs_dict[scout_string])
                             models.storage.save()
@@ -138,78 +114,15 @@ def do_destroy(self, line):
         else:
             print("** class name missing **")
 
-def fill_custom_command(self, class_name, motion):
-        """Handling of custom commands like <class name>.all()
-        or <class name>.count()."""
-        units = motion.split("(")
-        if len(units) == 2 and units[1].endswith(')'):
-            motion_name = units[0]
-            motion_args = units[1][:-1].split(',')
-
-            # Striping of surrounding quotes if present
-            motion_args = [arg.strip('\"') for arg in motion_args]
-
-            if motion_name == 'show':
-                key = "{}.{}".format(class_name, motion_args[0])
-                if key in models.storage.all():
-                    print(models.storage.all()[key])
-                else:
-                    print(f"** no instance found **")
-            elif motion_name == 'all':
-                instances = [
-                    str(obj) for key, obj in models.storage.all().items()
-                    if key.startswith(class_name + '.')
-                ]
-                print(instances)
-            elif motion_name == 'count':
-                count = sum(
-                    1 for key in models.storage.all()
-                    if key.startswith(class_name + '.')
-                )
-                print(count)
-            elif motion_name == 'destroy':
-                key = "{}.{}".format(class_name, motion_args[0])
-                if key in models.storage.all():
-                    del models.storage.all()[key]
-                    models.storage.save()
-                else:
-                    print(f"** no instance found **")
-            elif motion_name == 'update':
-                key = "{}.{}".format(class_name, motion_args[0])
-                if key in models.storage.all():
-                    obj = models.storage.all()[key]
-                    attribute_name = motion_args[1]
-                    attribute_value = motion_args[2]
-
-                    # Updating the attribute with the given value
-                    setattr(obj, attribute_name, attribute_value)
-                    obj.save()
-                else:
-                    print(f"** no instance found **")
-            else:
-                print(f"Unrecognized action: {motion_name}.\
-                Type 'help' for assistance.\n")
-        else:
-            print(f"Unrecognized action: {motion}.\
-            Type 'help' for assistance.\n")
-
-def default(self, line):
-        """Handle unrecognized commands."""
-        units = line.split('.')
-        if len(units) == 2:
-            class_name, motion = units
-            self.fill_custom_command(class_name, motion)
-        else:
-            print(f"Unrecognized command: {line}.\
-                  Type 'help' for assistance.\n")
-def do_all(self, line):
+    def do_all(self, line):
         """
         Deletes an instance based on the class name and id
         (save the change into the JSON file)
         """
         staged_classes = [
-                "BaseModel", "User", "State",
-                "City", "Amenity", "Place", "Review"]
+            "BaseModel", "User", "State", "City",
+            "Amenity", "Place", "Review"
+        ]
         if len(line) > 0:
             line_array = line.split()
             if len(line_array) > 0:
@@ -228,14 +141,15 @@ def do_all(self, line):
                 full_list.append(str(value))
             print(full_list)
 
-def do_update(self, line):
+    def do_update(self, line):
         """
         Updates an instance based on the class name and id by adding
         or updating attribute (save the change into the JSON file)
         """
         staged_classes = [
-                "BaseModel", "User", "State",
-                "City", "Amenity", "Place", "Review"]
+            "BaseModel", "User", "State", "City",
+            "Amenity", "Place", "Review"
+        ]
         if len(line) > 0:
             line_array = line.split()
             if len(line_array) > 0:
@@ -244,16 +158,18 @@ def do_update(self, line):
                     if len(line_array) > 1:
                         objs_dict = models.storage.all()
                         scout_string = "{}.{}".format(
-                                class_name, line_array[1])
+                            class_name, line_array[1])
                         if scout_string in objs_dict:
                             if len(line_array) > 2:
                                 if len(line_array) > 3:
                                     if (line_array[3]
                                             not in
                                             ["created_at",
-                                                "updated_at", "id"]):
-                                        setattr(objs_dict[scout_string], str(
-                                            line_array[2]), str(line_array[3]))
+                                             "updated_at", "id"]):
+                                        setattr(
+                                            objs_dict[scout_string],
+                                            str(line_array[2]),
+                                            str(line_array[3]))
                                 else:
                                     print("** value missing **")
                             else:
@@ -266,59 +182,6 @@ def do_update(self, line):
                     print("** class doesn't exist **")
         else:
             print("** class name missing **")
-
-def data_model_func(self, line, class_name):
-        """
-        handles all data models
-        """
-        allowed_methods = [".all()", ".count()"]
-        show_regex = re.compile(r"\.show\(\"(.*?)\"\)")
-        delete_regex = re.compile(r"\.destroy\(\"(.*?)\"\)")
-        update_regex = re.compile(r"\.update\(\"(.*?)\", \"(.*?)\", (.*?)\)")
-        update_dict_regex = re.compile(r"\.update\(\"(.*?)\",(.*?)\)")
-        if len(line) > 0:
-            line_array = line.split()
-            if len(line_array) > 0:
-                command_method = line_array[0]
-                if command_method in allowed_methods:
-                    if command_method == ".all()":
-                        self.do_all(class_name)
-                    if command_method == ".count()":
-                        self.get_count(class_name)
-                elif (show_regex.search(line_array[0]) is not None):
-                    obj_id = show_regex.search(line_array[0]).group(1)
-                    self.do_show("{} {}".format(class_name, obj_id))
-                elif (delete_regex.search(line_array[0]) is not None):
-                    obj_id = delete_regex.search(line_array[0]).group(1)
-                    self.do_destroy("{} {}".format(class_name, obj_id))
-                elif (update_regex.search(line) is not None):
-                    obj_id = update_regex.search(line).group(1)
-                    obj_attr_name = update_regex.search(line).group(2)
-                    obj_attr_value = update_regex.search(line).group(3)
-                    self.do_update("{} {} {} {}".format(
-                        class_name, obj_id, obj_attr_name, obj_attr_value))
-                elif (update_dict_regex.search(line) is not None):
-                    obj_id = update_dict_regex.search(line).group(1)
-                    obj_dict = eval(update_dict_regex.search(line).group(2))
-                    for key, value in obj_dict.items():
-                        self.do_update("{} {} {} {}".format(
-                            class_name, obj_id, key, value))
-
-def get_count(self, class_name):
-        """
-        Account for full count of all the dataset
-        """
-        staged_classes = [
-                "BaseModel", "User", "State",
-                "City", "Amenity", "Place", "Review"]
-        if class_name in staged_classes:
-            full_list = []
-            for key, value in models.storage.all().items():
-                if (class_name in key):
-                    full_list.append(str(value))
-            print(len(full_list))
-        else:
-            print("** class doesn't exist **")
 
 
 if __name__ == '__main__':

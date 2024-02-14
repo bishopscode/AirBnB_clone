8afa35aea379bab47b56efb856c7a714a46deaac
bishopscode <64908269+bishopscode@users.noreply.github.com>
2024-02-14 12:15:16 +0100

    Update console.py

--- console.py
+++ console.py
@@ -1,184 +1,38 @@
 #!/usr/bin/python3
-"""
-This module defines a command interpreter class for the AirBnB project.
-"""
-import cmd
-import shlex
-import models
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.place import Place
-from models.amenity import Amenity
-from models.review import Review
 
+import sys
+import unittest
 
-class HBNBCommand(cmd.Cmd):
-    """
-    Command interpreter class
-    """
-    prompt = "(hbnb) "
+class TestMyCode(unittest.TestCase):
 
-    def do_create(self, line):
-        """
-        Creates a new instance of BaseModel, saves it (to the JSON file)
-        and prints the id.
-        """
-        if not line:
-            print("** class name missing **")
-            return
+    def test_create_command(self):
+        # Test the create command
+        result = ("create User")
+        self.assertIn("User", result)
+        self.assertIn("id", result)
 
-        class_name = line.split()[0]
-        if class_name not in ['BaseModel', 'User', 'State', 'City', 'Place', 'Amenity', 'Review']:
-            print("** class doesn't exist **")
-            return
+    def test_show_command(self):
+        # Test the show command
+        result = ("show User 1234")
+        self.assertIn("User", result)
+        self.assertIn("id", result)
+        self.assertIn("1234", result)
 
-        new_instance = eval(class_name)()
-        new_instance.save()
-        print(new_instance.id)
+    def test_destroy_command(self):
+        # Test the destroy command
+        result = ("destroy User 1234")
+        self.assertIn("Deleted", result)
 
-    def do_show(self, line):
-        """
-        Prints the string representation of an instance
-        based on the class name and id.
-        """
-        if not line:
-            print("** class name missing **")
-            return
+    def test_all_command(self):
+        # Test the all command
+        result = ("all User")
+        self.assertIn("User", result)
+        self.assertIn("id", result)
 
-        args = shlex.split(line)
-        if args[0] not in ['BaseModel', 'User', 'State', 'City', 'Place', 'Amenity', 'Review']:
-            print("** class doesn't exist **")
-            return
-        elif len(args) == 1:
-            print("** instance id missing **")
-            return
-
-        key = args[0] + '.' + args[1]
-        all_objs = models.storage.all()
-        if key in all_objs:
-            print(all_objs[key])
-        else:
-            print("** no instance found **")
-
-    def do_destroy(self, line):
-        """
-        Deletes an instance based on the class name and id.
-        """
-        if not line:
-            print("** class name missing **")
-            return
-
-        args = shlex.split(line)
-        if args[0] not in ['BaseModel', 'User', 'State', 'City', 'Place', 'Amenity', 'Review']:
-            print("** class doesn't exist **")
-            return
-        elif len(args) == 1:
-            print("** instance id missing **")
-            return
-
-        key = args[0] + '.' + args[1]
-        all_objs = models.storage.all()
-        if key in all_objs:
-            del all_objs[key]
-            models.storage.save()
-        else:
-            print("** no instance found **")
-
-    def do_all(self, line):
-        """
-        Prints all string representation of all instances
-        based or not on the class name.
-        """
-        all_objs = models.storage.all()
-        if not line:
-            print([str(obj) for obj in all_objs.values()])
-        elif line not in ['BaseModel', 'User', 'State', 'City', 'Place', 'Amenity', 'Review']:
-            print("** class doesn't exist **")
-        else:
-            print([str(obj) for obj in all_objs.values() if type(obj).__name__ == line])
-
-    def do_update(self, line):
-        """
-        Updates an instance based on the class name and id
-        by adding or updating attribute.
-        """
-        if not line:
-            print("** class name missing **")
-            return
-
-        args = shlex.split(line)
-        if args[0] not in ['BaseModel', 'User', 'State', 'City', 'Place', 'Amenity', 'Review']:
-            print("** class doesn't exist **")
-            return
-        elif len(args) == 1:
-            print("** instance id missing **")
-            return
-        elif len(args) == 2:
-            print("** attribute name missing **")
-            return
-        elif len(args) == 3:
-            print("** value missing **")
-            return
-
-        key = args[0] + '.' + args[1]
-        all_objs = models.storage.all()
-        if key not in all_objs:
-            print("** no instance found **")
-            return
-
-        setattr(all_objs[key], args[2], args[3])
-        models.storage.save()
-
-    def emptyline(self):
-        """
-        Called when an empty line is entered in response to the prompt.
-        Does nothing by default.
-        """
-        pass
-
-    def do_EOF(self, line):
-        """
-        Handles the EOF signal to exit the program.
-        """
-        print()
-        return True
-
-    def do_quit(self, line):
-        """
-        Quit command to exit the program.
-        """
-        return True
-
-    def precmd(self, line):
-        """
-        This method is called after the input prompt is displayed
-        but before the input is read and interpreted.
-        """
-        if '.' not in line:
-            return line
-        else:
-            units = line.split('.')
-            class_name = units[0]
-            motion = units[1]
-
-            if motion == "all()":
-                return class_name + " all"
-            elif motion[:4] == "show":
-                return class_name + " " + motion[5:-2]
-            elif motion[:7] == "destroy":
-                return class_name + " " + motion[8:-2]
-            elif motion[:6] == "update":
-                rest = motion[7:]
-                idx = rest.find('"') + 1
-                attribute = rest[:idx]
-                rest = rest[idx + 1:].strip()
-                idx = rest.find('"') + 1
-                value = rest[:idx]
-                return class_name + " " + rest[idx + 1:-1] + " " + attribute + " " + value
-            else:
-                return line
+    def test_update_command(self):
+        # Test the update command
+        result = ("update User 1234 name John")
+        self.assertIn("Updated", result)
 
 if __name__ == '__main__':
-    HBNBCommand().cmdloop()
+    unittest.main()
